16ca1d129f5cff876b28965dbde124b7815c78b5
201st-Luka <luka.eichten@gmail.com>
2023-07-07 23:11:58 +0200

    Location.py doc

--- pyclasher/models/Location.py
+++ pyclasher/models/Location.py
@@ -2,33 +2,27 @@ from .BaseModels import IterBaseModel, BaseModel
 
 
 class Location(BaseModel):
-    def __init__(self, data: dict | None):
+    def __init__(self, data):
         super().__init__(data)
         self._main_attribute = self.id if data is not None else None
         return
 
     @property
-    def localized_name(self) -> str:
+    def localized_name(self):
         return self._get_data('localizedName')
 
     @property
-    def id(self) -> int:
+    def id(self):
         return self._get_data('id')
 
     @property
-    def name(self) -> str:
+    def name(self):
         return self._get_data('name')
 
     @property
-    def country_code(self) -> bool:
+    def country_code(self):
         return self._get_data('countryCode')
 
 
 class LocationList(IterBaseModel):
     _iter_rtype = Location
-
-    def __getitem__(self, item: int | str) -> _iter_rtype:
-        return super().__getitem__(item)
-
-    def __next__(self) -> _iter_rtype:
-        return super().__next__()
---
+++ pyclasher/models/Location.pyi
@@ -0,0 +1,69 @@
+"""
+models concerning the location
+"""
+
+
+from .BaseModels import IterBaseModel, BaseModel
+from ..Exceptions import Missing
+
+
+class Location(BaseModel):
+    """
+    location model
+    """
+
+    @property
+    def localized_name(self) -> str:
+        """
+        localized name of the location
+
+        :return:    the location's localized name
+        :rtype:     str
+        """
+        ...
+
+    @property
+    def id(self) -> int:
+        """
+        location ID
+
+        :return:    the ID of the location
+        :rtype:     int
+        """
+        ...
+
+    @property
+    def name(self) -> str:
+        """
+        location name
+
+        :return:    the name of the location
+        :rtype:     str
+        """
+        ...
+
+    @property
+    def country_code(self) -> int | Missing:
+        """
+        location country code
+
+        :return:    the country code of the location if available
+        :rtype:     int | MISSING
+        """
+        ...
+
+
+class LocationList(IterBaseModel):
+    """
+    location list model
+
+    can be iterated over
+    """
+
+    _iter_rtype = Location
+
+    def __getitem__(self, item: int | str) -> _iter_rtype:
+        ...
+
+    def __next__(self) -> _iter_rtype:
+        ...
