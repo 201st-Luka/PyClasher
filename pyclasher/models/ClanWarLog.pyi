d22d89f25b6c4c9cf3a9b60033b7d4016a44e73e
201st-Luka <luka.eichten@gmail.com>
2023-07-07 12:12:41 +0200

    ClanWarLog.py doc

---
+++ pyclasher/models/ClanWarLog.pyi
@@ -0,0 +1,85 @@
+from .BaseModels import IterBaseModel, BaseModel, Time
+from .Enums import ClanWarResult
+from .WarClan import WarClan
+
+
+class ClanWarLogEntry(BaseModel):
+    """
+    clan war log entry model
+    """
+
+    @property
+    def clan(self) -> WarClan:
+        """
+        clan of the clan war
+
+        :return:    the clan of the clan war
+        :rtype:     WarClan
+        """
+        ...
+
+    @property
+    def opponent(self) -> WarClan:
+        """
+        opponent of the clan war
+
+        :return:    the opponent of the clan war
+        :rtype:     WarClan
+        """
+        ...
+
+    @property
+    def team_size(self) -> int:
+        """
+        clan war team size
+
+        :return:    the clan war team size
+        :rtype:     int
+        """
+        ...
+
+    @property
+    def attacks_per_member(self) -> int:
+        """
+        attack count per member
+
+        :return:    the attack count per member (usually 2 for regular war and 1 for clan war league)
+        :rtype:     int
+        """
+        ...
+
+    @property
+    def end_time(self) -> Time:
+        """
+        end time of the clan war
+
+        :return:    the end time of the clan war
+        :rtype:     Time
+        """
+        ...
+
+    @property
+    def result(self) -> ClanWarResult:
+        """
+        result of the clan war
+
+        :return:    the result of the clan war
+        :rtype:     ClanWarResult
+        """
+        ...
+
+
+class ClanWarLog(IterBaseModel):
+    """
+    clan war log model
+
+    can be iterated over
+    """
+
+    _iter_rtype = ClanWarLogEntry
+
+    def __getitem__(self, item: int) -> _iter_rtype:
+        ...
+
+    def __next__(self) -> _iter_rtype:
+        ...
