52d21495e0b5e555be3c877aa235b7880719b8a3
201st-Luka <luka.eichten@gmail.com>
2023-08-13 17:50:27 +0200

    test: implemented label tests

---
+++ tests/requests/async_tests/test_labels.py
@@ -0,0 +1,40 @@
+import pytest
+
+from pyclasher import ClanLabelsRequest, PlayerLabelsRequest
+from pyclasher.models import LabelList, Paging, IconUrls
+
+
+@pytest.mark.asyncio
+async def test_clan_labels(event_loop, pyclasher_client):
+    clan_labels = ClanLabelsRequest()
+
+    await clan_labels.request()
+
+    assert isinstance(clan_labels.to_dict(), dict)
+    assert isinstance(clan_labels.items, LabelList)
+    assert isinstance(clan_labels.paging, Paging)
+
+    for label in clan_labels:
+        assert isinstance(label.to_dict(), dict)
+        assert isinstance(label.name, str)
+        assert isinstance(label.id, int)
+        assert isinstance(label.icon_urls, IconUrls)
+
+
+@pytest.mark.asyncio
+async def test_clan_labels(event_loop, pyclasher_client):
+    player_labels = PlayerLabelsRequest()
+
+    await player_labels.request()
+
+    assert isinstance(player_labels.to_dict(), dict)
+    assert isinstance(player_labels.items, LabelList)
+    assert isinstance(player_labels.paging, Paging)
+
+    for label in player_labels:
+        assert isinstance(label.to_dict(), dict)
+        assert isinstance(label.name, str)
+        assert isinstance(label.id, int)
+        assert isinstance(label.icon_urls, IconUrls)
+
+
