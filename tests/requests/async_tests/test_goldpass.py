c5c54c7e9a9e519ae1564eef06ed3fe96a5aef19
201st-Luka <luka.eichten@gmail.com>
2023-08-13 19:48:51 +0200

    test: `GoldPassRequest` test

---
+++ tests/requests/async_tests/test_goldpass.py
@@ -0,0 +1,15 @@
+import pytest
+
+from pyclasher import GoldPassRequest
+from pyclasher.models import Time
+
+
+@pytest.mark.asyncio
+async def test_goldpass(event_loop, pyclasher_client):
+    goldpass = GoldPassRequest()
+
+    await goldpass.request()
+
+    assert isinstance(goldpass.to_dict(), dict)
+    assert isinstance(goldpass.start_time, Time)
+    assert isinstance(goldpass.end_time, Time)
