d4e92384c64e8298a70195117222ed01b3197e37
201st-Luka <luka.eichten@gmail.com>
2023-08-13 17:04:23 +0200

    fix: `Player.league` can be MISSING

--- pyclasher/models/Player.pyi
+++ pyclasher/models/Player.pyi
@@ -1,5 +1,6 @@
 from typing import Iterator
 
+from ..Exceptions import Missing
 from .BaseModels import BaseModel, IterBaseModel, BaseClan
 from .Enums import ClanRole, WarPreference, Village
 from .Labels import LabelList
@@ -175,7 +176,7 @@ class PlayerAchievementProgressList(IterBaseModel):
 
 class Player(BaseModel):
     @property
-    def league(self) -> League:
+    def league(self) -> Missing | League:
         ...
 
     @property
