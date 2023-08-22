30e7df600151fa04559c65c5438014a78540073e
201st-Luka <luka.eichten@gmail.com>
2023-08-22 21:53:37 +0200

    feat: implemented `ClanWarleaguesWarsRequest`

---
+++ pyclasher/api/requests/clan_warleagues_wars.py
@@ -0,0 +1,14 @@
+from .abc import RequestModel
+from ..models import ClanWarLeagueGroup
+
+
+class ClanWarleaguesWarsRequest(RequestModel, ClanWarLeagueGroup):
+    """
+    Retrieve information about individual clan war league war
+    """
+
+    def __init__(self, war_tag):
+        self.war_tag = war_tag
+        super().__init__("clanwarleagues/wars/{war_tag}", war_tag=self.war_tag)
+        self._main_attribute = self.war_tag
+        return
