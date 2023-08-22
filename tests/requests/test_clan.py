60fad416df1de03048a8a032e9193682c04cd42f
201st-Luka <luka.eichten@gmail.com>
2023-08-22 19:02:25 +0200

    tests: fixed crash

--- tests/requests/test_clan.py
+++ tests/requests/test_clan.py
@@ -1,15 +1,19 @@
 import pytest
 
-from pyclasher import (ClanRequest, ClanMembersRequest, Missing, ClanCurrentWarRequest, ClanWarLogRequest,
-                       ClanSearchRequest, ClanCapitalRaidSeasonsRequest)
-from pyclasher.api.models import (ClanType, WarFrequency, BadgeUrls, WarLeague, CapitalLeague, Language, ClanCapital,
-                                  LabelList, Location, ClanMemberList, Paging, BuilderBaseLeague, League, PlayerHouse,
-                                  ClanRole, ClanMember, ClanWarState, WarClan, Time, ClanWarLog, ClanWarResult,
-                                  ClanList,
-                                  ClanCapitalRaidSeasons, ClanCapitalRaidSeasonMemberList,
-                                  ClanCapitalRaidSeasonAttackLogList, ClanCapitalRaidSeasonDefenseLogList)
+from pyclasher import (
+    ClanRequest, ClanMembersRequest, Missing, ClanCurrentWarRequest,
+    ClanWarLogRequest, ClanSearchRequest, ClanCapitalRaidSeasonsRequest
+)
+from pyclasher.api.models import (
+    ClanType, WarFrequency, BadgeUrls, WarLeague, CapitalLeague, Language,
+    ClanCapital, LabelList, Location, ClanMemberList, Paging,
+    BuilderBaseLeague, League, PlayerHouse, ClanRole, ClanMember,
+    ClanWarState, WarClan, Time, ClanWarLog, ClanWarResult, ClanList,
+    ClanCapitalRaidSeasons, ClanCapitalRaidSeasonMemberList,
+    ClanCapitalRaidSeasonAttackLogList, ClanCapitalRaidSeasonDefenseLogList
+)
 
-from constants import TEST_CLAN_TAG, TEST_CLAN_NAME
+from ..constants import TEST_CLAN_TAG, TEST_CLAN_NAME
 
 
 @pytest.mark.asyncio
