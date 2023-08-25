1336f8b2b0ff0401da3a05842f018ccafe4326b5
201st-Luka <luka.eichten@gmail.com>
2023-08-25 21:31:06 +0200

    tests: implemented league tests

---
+++ pyclasher/api/requests/league_seasons.pyi
@@ -0,0 +1,25 @@
+from typing import Iterator
+
+from .abc import IterRequestModel
+from ..models import LeagueSeason, LeagueSeasonList
+
+
+class LeagueSeasonsRequest(IterRequestModel):
+    _iter_rtype = LeagueSeason
+    _list_rtype = LeagueSeasonList
+
+    def __init__(self, league_id: int):
+        ...
+
+    @property
+    def items(self) -> _list_rtype:
+        ...
+
+    def __getitem__(self, item: int) -> _iter_rtype:
+        ...
+
+    def __iter__(self) -> Iterator[_iter_rtype]:
+        ...
+
+    def __next__(self) -> _iter_rtype:
+        ...
