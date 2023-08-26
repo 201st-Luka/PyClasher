aed383508058f8e3c32acbb7821b27d0f819f26f
201st-Luka <luka.eichten@gmail.com>
2023-08-26 11:17:43 +0200

    feat: implemented `CapitalRankingsRequest`

---
+++ pyclasher/api/requests/capital_rankings.pyi
@@ -0,0 +1,27 @@
+from typing import Iterator
+
+from .abc import IterRequestModel
+from ..models import ClanCapitalRanking, ClanCapitalRankingList, Location
+
+
+class CapitalRankingsRequest(IterRequestModel):
+    _iter_rtype = ClanCapitalRanking
+    _list_rtype = ClanCapitalRankingList
+
+    def __init__(self, location_id: int | Location,
+                 limit: int = None, after: str = None, before: str = None):
+        self.location_id: int = ...
+        ...
+
+    @property
+    def items(self) -> _list_rtype:
+        ...
+
+    def __getitem__(self, item: int) -> _iter_rtype:
+        ...
+
+    def __iter__(self) -> Iterator[_iter_rtype]:
+        ...
+
+    def __next__(self) -> _iter_rtype:
+        ...
