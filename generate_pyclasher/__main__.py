21eae1b0598e950a09b772af4ab9c1782a6bfd64
201st-Luka <81482503+201st-Luka@users.noreply.github.com>
2024-07-16 20:24:43 +0200

    add comments

--- generate_pyclasher/__main__.py
+++ generate_pyclasher/__main__.py
@@ -10,6 +10,7 @@ from yaml import safe_load
 from . import get_yaml, generate
 
 if __name__ == '__main__':
+    # parse arguments
     parser = ArgumentParser(
         prog="generate_pyclasher",
         description="Generate the API models of the Clash of Clans API wrapper",
@@ -21,8 +22,10 @@ if __name__ == '__main__':
 
     parsed_args = parser.parse_args()
 
+    # get path
     generate_path = parsed_args.path
 
+    # get email and password
     email, password = environ.get("EMAIL") or parsed_args.email, environ.get("PASSWORD") or parsed_args.password
 
     if not email:
@@ -31,14 +34,18 @@ if __name__ == '__main__':
         exit("Password is required, provide it as an argument or as an environment variable ('PASSWORD').")
 
     with NamedTemporaryFile("w", encoding="utf-8", delete_on_close=False) as tmp_file:
+        # get yaml file
         run(get_yaml(email, password, tmp_file.name))
         tmp_file.close()
 
+        # delete old generated files and create the directory
         if exists(generate_path):
             rmtree(generate_path)
         makedirs(generate_path)
 
+        # load yaml file
         with open(tmp_file.name, "r", encoding="utf-8") as file:
             yaml_file = safe_load(file)
 
+        # generate the models
         generate(yaml_file, generate_path)
