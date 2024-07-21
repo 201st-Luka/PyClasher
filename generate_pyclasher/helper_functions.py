254ce4426acda6b8077bef6c6916aff13708e7c5
201st-Luka <81482503+201st-Luka@users.noreply.github.com>
2024-07-21 17:40:12 +0200

    helper_functions.py

--- generate_pyclasher/helper_functions.py
+++ generate_pyclasher/helper_functions.py
@@ -8,3 +8,26 @@ def convert_operation_id(operation_id: str) -> str:
 
 def camel_to_snake_case(camel_case: str) -> str:
     return "".join(["_" + i.lower() if i.isupper() else i for i in camel_case]).lstrip("_")
+
+
+def find_array_sub_definition(type_, definitions) -> str:
+    definition = definitions[type_]
+
+    assert definition["type"] == "array"
+
+    items = definition["items"]
+
+    if "$ref" in items:
+        return items["$ref"].removeprefix("#/definitions/")
+    else:
+        return items["type"]
+
+
+def format_request_url(raw_url: str) -> str:
+    try:
+        start = raw_url.find("{")
+        end = raw_url.find("}") + 1
+    except ValueError:
+        return raw_url
+    else:
+        return raw_url[:start] + camel_to_snake_case(raw_url[start:end]) + raw_url[end:]
