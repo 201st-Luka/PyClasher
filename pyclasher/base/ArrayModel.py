e8f92ed40716c3fb3294a37479193a9cf3c359ad
201st-Luka <81482503+201st-Luka@users.noreply.github.com>
2024-07-22 15:23:44 +0200

    improved ArrayModel

--- pyclasher/base/ArrayModel.py
+++ pyclasher/base/ArrayModel.py
@@ -4,7 +4,7 @@
 
 from typing import Iterator
 
-from ..exceptions import Missing
+from ..exceptions import Missing, RequestNotDone
 
 
 class ArrayModel[T]:
@@ -12,25 +12,35 @@ class ArrayModel[T]:
     Class to iterate over an array of items
 
     Attributes:
-        _items (list[dict]):
+        _items (list[dict | str | int | float | bool]):
             Array of items
         _type (type[T]):
             Type of the items in the array
     """
 
-    def __init__(self, data: dict[str, list[dict]] | Missing | None, type_: type[T]) -> None:
+    def __init__(self, items: list[dict | str | int | float | bool] | Missing | None, type_: type[T]) -> None:
         """
         Args:
-            data (dict[str, list[dict]]):
+            items (list[dict]):
                 Data dictionary of the API response
             type_ (type[T]):
                 Type of the items in the array
         """
-        self._items = data["items"]
+        self._items = items
         self._type = type_
 
+    def _get_items(self) -> list[T]:
+        """
+        Check if the items are present
+        """
+        if self._items is Missing:
+            raise RequestNotDone
+        if self._items is None:
+            return []
+        return self._items
+
     def __iter__(self) -> Iterator[T]:
-        return (self._type(item) for item in self._items)
+        return (self._type(item) for item in self._get_items())
 
     def __getitem__(self, index: int) -> T:
         """
@@ -41,13 +51,21 @@ class ArrayModel[T]:
             T:
                 Item at the index
         """
-        return self._type(self._items[index])
+        return self._type(self._get_items()[index])
 
     def __len__(self) -> int:
-        return len(self._items)
+        return len(self._get_items())
 
     def __str__(self) -> str:
+        if self._items is Missing:
+            return f"{self.__class__.__name__}(Request not done)"
+        if self._items is None:
+            return f"{self.__class__.__name__}(No items of type {self._type})"
         return f"{self.__class__.__name__}({len(self)} items of type {self._type})"
 
     def __repr__(self) -> str:
+        if self._items is Missing:
+            return f"<{self.__class__.__name__}(Request not done)>"
+        if self._items is None:
+            return f"<{self.__class__.__name__}(No items of type {self._type})>"
         return f"<{self.__class__.__name__}([{', '.join((repr(item)) for item in self)}])>"
